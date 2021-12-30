5ddbc57e3f66b30076d880ccbd550e3c1a12440f
Devabhaik <dbhaik1991@gmail.com>
2021-12-30 21:01:09 +0530

    count

--- src/Count.js
+++ src/Count.js
@@ -3,14 +3,30 @@ import './Count.css';
 
 const Count = () => {
   const [count, setCount] = useState(0);
+
+  const Increament = () => {
+    setCount(count + 1);
+  };
+  const Decreament = () => {
+    setCount(count - 1);
+    if (count <= 0) {
+      setCount(0);
+    }
+  };
+  const Reset = () => {
+    setCount(0);
+  };
+
   return (
     <>
       <div className="count">
-        <h1>{count}</h1>
-        <div>
-          <button>Decreament</button>
-          <button>Reset</button>
-          <button>Increament</button>
+        <div className="container">
+          <p>{count}</p>
+          <div className="button">
+            <button onClick={Decreament}>Decreament</button>
+            <button onClick={Reset}>Reset</button>
+            <button onClick={Increament}>Increament</button>
+          </div>
         </div>
       </div>
     </>
